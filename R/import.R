4dadcbcf61421908106d7c8c952193c2f456f7bf
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-11-03 17:29:21 -0500

    use smaller json.gz file in example

--- R/import.R
+++ R/import.R
@@ -109,7 +109,7 @@ setMethod("show", "HoverJSON", function(object) {
 #' @examplesIf interactive()
 #' hov_json_file <- paste0(
 #'     "https://store.cancerdatasci.org/hovernet/TCGA_OV/json/",
-#'     "TCGA-23-1121-01Z-00-DX1.E2F25441-32C3-46BF-A845-CB4FA787E8CB.json.gz"
+#'     "TCGA-VG-A8LO-01A-01-DX1.B39A4D64-82A1-4A04-8AB6-918F3058B83B.json.gz"
 #' )
 #' dest_json <- file.path(tempdir(), basename(hov_json_file))
 #' download.file(hov_json_file, destfile = dest_json)
