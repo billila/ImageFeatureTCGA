bd6a41241d016b6cda127fbba7c32661320d3667
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-01-29 16:54:54 -0500

    add tile_assay as SummarizedExperiment with BumpyMatrix

--- R/linkTCGA.R
+++ R/linkTCGA.R
@@ -6,6 +6,8 @@
 #' @param MultiAssayExperiment A `MultiAssayExperiment` object containing sample
 #'   metadata with TCGA barcodes.
 #'
+#' @importFrom S4Vectors DataFrame
+#'
 #' @examplesIf interactive()
 #' library(curatedTCGAData)
 #' coad <- curatedTCGAData(
@@ -33,14 +35,22 @@ linkTCGA <- function(
     ) |>
         import(redownload = redownload, parallel = parallel)
     slide_assay <- .slide_df_to_se(resdata[["slide_level"]])
-    sampmap <- DataFrame(
+    slide_sm <- DataFrame(
         assay = "slide_assay",
         primary = metadata(slide_assay)[["patientIds"]],
         colname = metadata(slide_assay)[["sampleIds"]]
     )
+    tile_assay <- .tile_df_to_bumpy_se(resdata[["tile_level"]])
+    tile_sm <- DataFrame(
+        assay = "tile_assay",
+        primary = metadata(tile_assay)[["patientIds"]],
+        colname = metadata(tile_assay)[["sampleIds"]]
+    )
+    sampmap <- rbind(slide_sm, tile_sm)
     c(
         MultiAssayExperiment,
         slide_assay = slide_assay,
+        tile_assay = tile_assay,
         sampleMap = sampmap
     )
 }
@@ -68,6 +78,48 @@ linkTCGA <- function(
         assays = list(embeddings = embeddings),
         metadata = metadata
     )
+}
+
+.tile_df_to_bumpy_se <- function(tdf) {
+    meta <- c("slide_name", "tile_id", "tile_name", "tile_x", "tile_y")
+    mdat <- .tile_prep_meta(tdf[, meta])
+    sampleIds <- .slide_to_sampleId(tdf[["slide_name"]])
+    patientIds <- TCGAutils::TCGAbarcode(sampleIds)
+    metadata <- list(
+        metadata = mdat, sampleIds = sampleIds, patientIds = patientIds
+    )
+    ncols <- ncol(tdf) - length(meta)
+    nsamps <- length(unique(sampleIds))
+    enames <- tdf[["slide_name"]] |>
+        as.factor() |>
+        levels() |>
+        .slide_to_sampleId()
+    split(
+        tdf[, -which(colnames(tdf) %in% meta)],
+        tdf[["slide_name"]]
+    ) |>
+        lapply(function(x) unname(x) |> IRanges::NumericList()) |>
+        unname() |>
+        do.call(c, args = _) |>
+        BumpyMatrix::BumpyMatrix(
+            dim = c(ncols, length(enames)),
+            dimnames = list(NULL, enames)
+        ) |>
+        list(tiles = _) |>
+        SummarizedExperiment(
+            metadata = metadata
+        )
+}
+
+.tile_prep_meta <- function(metadf) {
+    metadf[["tile_x"]] <- .rm_tensor_txt(metadf[["tile_x"]])
+    metadf[["tile_y"]] <- .rm_tensor_txt(metadf[["tile_y"]])
+    metadf
+}
+
+.rm_tensor_txt <- function(txt) {
+    gsub("tensor\\((.*)\\.\\)", "\\1", x = txt) |>
+        as.numeric()
 }
 
 .slide_to_sampleId <- function(txt) {
