b81cd6e364aa78da12f14da851e3fcc4c4dfdc4f
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-02-10 17:37:51 -0500

    minor formatting of getEmbeddings arguments

--- R/ProvGigaList.R
+++ R/ProvGigaList.R
@@ -195,8 +195,9 @@ setMethod("import", "ProvGigaList", function(con, format, text, ...) {
 #'   embeddings from all `ProvGiga` objects within a `ProvGigaList`.
 #'
 #' @export
-getEmbeddings <- function(con, parallel = TRUE,
-                            layer = "last_layer_embed", ...) {
+getEmbeddings <- function(
+    con, parallel = TRUE, layer = "last_layer_embed", ...
+) {
     if (!is(con, "ProvGigaList"))
         con <- ProvGigaList(con, ...)
 
